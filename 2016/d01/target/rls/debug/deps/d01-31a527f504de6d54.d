7071eea4e401e3237891860e97e042fda3582cd8
Kai Rosswag <kairosswag@gmail.com>
2017-11-28 17:17:02 +0100

    Setting up cargo and Input.

---
+++ 2016/d01/target/rls/debug/deps/d01-31a527f504de6d54.d
@@ -0,0 +1,5 @@
+d:\dev\aoc\2016\d01\target\rls\debug\deps\d01-31a527f504de6d54.rmeta: src\main.rs
+
+d:\dev\aoc\2016\d01\target\rls\debug\deps\d01-31a527f504de6d54.d: src\main.rs
+
+src\main.rs:
---
+++ 2016/d01/target/rls/debug/libd01.d
@@ -0,0 +1,1 @@
+d:\dev\aoc\2016\d01\target\rls\debug\libd01.rmeta: d:\dev\aoc\2016\d01\src\main.rs
